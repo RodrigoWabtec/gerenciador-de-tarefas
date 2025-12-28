c7cd023dff2ed5ab9f4a2752ab45405b57ca3470
Anderson Rodrigo Diniz Oliveira <anderson.diniz@discente.ufma.br>
2025-12-28 14:40:07 -0300

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+while True:
+    print("\n1 - Adicionar tarefa")
+    print("2 - Listar tarefas")
+    print("3 - Alterar status")
+    print("4 - Remover tarefa")
+    print("0 - Sair")
+
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        print("1")
+
+    elif opcao == "2":
+        print("2")
+
+    elif opcao == "3":
+        print("3")
+
+    elif opcao == "4":
+        print("4")
+
+    elif opcao == "0":
+        break
